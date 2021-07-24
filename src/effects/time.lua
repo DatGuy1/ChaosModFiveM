255adcc5bf9456288daee8243fb3f74de25053ba
DatGuy <datguysteam@gmail.com>
2021-07-24 07:53:02 +0300

    pausechamp

--- src/effects/time.lua
+++ src/effects/time.lua
@@ -1,27 +1,35 @@
 -- Probably doesn't work
 RegisterNetEvent('Chaos:Time:Superhot', function(duration)
+    local exitMethod = false
     exports.helpers:DisplayMessage("Super. Hot.")
-    local lastCheck = 0
-    local timer = 0
-    while timer < duration do
-        local currentTime = GetGameTimer()
-        if currentTime - lastCheck > 100 then
-            lastCheck = currentTime;
-            local playerId = PlayerPedId();
-            local gameSpeed = 1.;
-            if not (IsPedGettingIntoAVehicle(playerId) or IsPedClimbing(playerId) or
-                IsPedDiving(playerId) or IsPedJumpingOutOfVehicle(playerId) or 
-                IsPedRagdoll(playerId) or IsPedGettingUp(playerId)) then
-                local speed = GetEntitySpeed(playerPed);
-                gameSpeed = math.max(math.min(speed, 4.) / 4, 0.2);
-            end
-            SetTimeScale(gameSpeed);
-        end
-        Citizen.Wait(1000 * gameSpeed)
-        timer = timer + 1
-    end
 
-    SetTimeScale(1.)
+    Citizen.SetTimeout(duration * 1000, function() exitMethod = true end)
+    Citizen.CreateThread(function()
+        ---@type integer
+        local lastCheck = 0
+        local timer = 0
+        while true do
+            if exitMethod then
+                break
+            end
+
+            local currentTime = GetGameTimer()
+            if currentTime - lastCheck > 100 then
+                lastCheck = currentTime;
+                local playerId = PlayerPedId();
+                local gameSpeed = 1.;
+                if not (IsPedGettingIntoAVehicle(playerId) or IsPedClimbing(playerId) or
+                        IsPedDiving(playerId) or IsPedJumpingOutOfVehicle(playerId) or
+                        IsPedRagdoll(playerId) or IsPedGettingUp(playerId)) then
+                    local speed = GetEntitySpeed(PlayerPedId());
+                    gameSpeed = math.max(math.min(speed, 4.) / 4, 0.2);
+                end
+                SetTimeScale(gameSpeed);
+            end
+
+            Citizen.Wait(0)
+        end
+    end)
 end)
 
 RegisterNetEvent('Chaos:Time:0.2x', function(duration)

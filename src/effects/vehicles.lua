c7f97409f52cbcc47c277efdf0883901280b6d21
DatGuy <datguysteam@gmail.com>
2021-07-25 09:27:19 +0300

    more events

--- src/effects/vehicles.lua
+++ src/effects/vehicles.lua
@@ -47,7 +47,7 @@ RegisterNetEvent('Chaos:Vehicles:AllVehsLaunchUp', function(duration)
     Citizen.CreateThread(function()
         for vehicle in exports.helpers:EnumerateVehicles() do
             local vehicleVelocity = GetEntityVelocity(vehicle)
-            SetEntityVelocity(vehicle, vehicleVelocity.x, vehicleVelocity.y, vehicleVelocity.z)
+            SetEntityVelocity(vehicle, vehicleVelocity.x, vehicleVelocity.y, 100.)
         end
     end)
 end)
@@ -87,9 +87,9 @@ RegisterNetEvent('Chaos:Vehicles:Beyblade', function(duration)
                     SetVehicleReduceGrip(vehicle, true)
                     if GetEntitySpeed(vehicle) < 10 then
                         local vehicleVelocity = GetEntitySpeedVector(vehicle, true)
-                        vehicleVelocity.x = vehicleVelocity * velocityMultiplier
-                        vehicleVelocity.y = vehicleVelocity * velocityMultiplier
-                        SetEntityVelocity(vehicle, vehicleVelocity.x, vehicleVelocity.y, vehicleVelocity.z)
+                        local newX = vehicleVelocity.x * velocityMultiplier
+                        local newY = vehicleVelocity.y * velocityMultiplier
+                        SetEntityVelocity(vehicle, newX, newY, vehicleVelocity.z)
                     end
                 else
                     SetVehicleReduceGrip(vehicle, false)
@@ -164,6 +164,7 @@ RegisterNetEvent('Chaos:Vehicles:BrakeBoost', function(duration)
                 break
             end
 
+            DisableControlAction(0, 72, true)
             if IsPedInAnyVehicle(playerPed, false) then
                 local playerVehicle = GetVehiclePedIsIn(playerPed, false)
                 if GetPedInVehicleSeat(playerVehicle, -1) == playerPed and IsControlPressed(0, 72) then
@@ -171,7 +172,7 @@ RegisterNetEvent('Chaos:Vehicles:BrakeBoost', function(duration)
                     if GetVehicleClass(playerVehicle) ~= 15 and GetEntityModel(playerVehicle) ~= "BLIMP" then
                         ApplyForceToEntity(
                                 playerVehicle, 0,
-                                .0, 50., .0,
+                                .0, 80., .0,
                                 .0, .0, .0,
                                 0, true, true, true, false, true
                         )
@@ -198,6 +199,121 @@ RegisterNetEvent('Chaos:Vehicles:BreakDoors', function(duration)
                     Citizen.Wait(0)
                 end
             end
+        end
+    end)
+end)
+
+RegisterNetEvent('Chaos:Vehicles:CinematicCam', function(duration)
+    local exitMethod = false
+    exports.helpers:DisplayMessage("Very artsy")
+
+    Citizen.SetTimeout(duration * 1000, function() exitMethod = true end)
+    Citizen.CreateThread(function()
+        while true do
+            if exitMethod then
+                break
+            end
+
+            SetPlayerCanDoDriveBy(PlayerId(), false)
+            SetCinematicModeActive(true)
+            DisableControlAction(0, 80, true)
+
+            if IsPedInAnyVehicle(PlayerPedId(), false) then
+                DisableControlAction(0, 27, true)
+                DisableControlAction(0, 0, true)
+            end
+
+            Citizen.Wait(0)
+        end
+
+        SetCinematicModeActive(false)
+        SetPlayerCanDoDriveBy(PlayerId(), true)
+    end)
+end)
+
+RegisterNetEvent('Chaos:Vehicles:CrumblingCars', function(duration)
+    local function GenRandomFloat()
+        return math.random(-10, 10) / 10.
+    end
+
+    local exitMethod = false
+    exports.helpers:DisplayMessage("Bono, my car is crumbling")
+
+    Citizen.SetTimeout(duration * 1000, function() exitMethod = true end)
+    Citizen.CreateThread(function()
+        while true do
+            if exitMethod then
+                break
+            end
+
+            local vehicles = {}
+            for vehicle in exports.helpers:EnumerateVehicles() do
+                table.insert(vehicles, vehicle)
+            end
+
+            if #vehicles > 0 then
+                SetVehicleDamage(
+                        vehicles[math.random(1, #vehicles)],
+                        GenRandomFloat(), GenRandomFloat(), GenRandomFloat(),
+                        ToFloat(math.random(10000, 100000) / 10),
+                        ToFloat(math.random(1000, 10000) / 10),
+                        true
+                )
+            end
+
+            Citizen.Wait(0)
+        end
+    end)
+end)
+
+RegisterNetEvent('Chaos:Vehicles:RainbowCars', function(duration)
+    local frequency = .1
+
+    local exitMethod = false
+    exports.helpers:DisplayMessage("Rainbow Cars!")
+
+    Citizen.SetTimeout(duration * 1000, function() exitMethod = true end)
+    Citizen.CreateThread(function()
+        local headlightColour = 0
+        -- Not sure what this actually does
+        local cnt = 0
+        while true do
+            if exitMethod then
+                break
+            end
+
+            cnt = cnt + 1
+            if cnt >= 4294967295 then
+                cnt = 0
+            end
+
+            headlightColour = headlightColour + 1
+            if headlightColour > 12 then
+                headlightColour = 0
+            end
+
+            for vehicle in exports.helpers:EnumerateVehicles() do
+                local newR = math.sin(vehicle + frequency * cnt) * 127 + 128
+                local newG = math.sin(vehicle + frequency * cnt + 2) * 127 + 128
+                local newB = math.sin(vehicle + frequency * cnt + 4) * 127 + 128
+
+                SetVehicleCustomPrimaryColour(vehicle, newR, newG, newB)
+                SetVehicleCustomSecondaryColour(vehicle, newG, newB, newR)
+
+                SetVehicleNeonLightsColour(vehicle, newR, newG, newB)
+                for i = 0, 3 do
+                    SetVehicleNeonLightEnabled(vehicle, i, true)
+                end
+
+                ToggleVehicleMod(vehicle, 22, true)
+                SetVehicleXenonLightsColor(vehicle, headlightColour)
+            end
+
+            Citizen.Wait(0)
+        end
+
+        for vehicle in exports.helpers:EnumerateVehicles() do
+            SetVehicleXenonLightsColor(vehicle, -1)
         end
     end)
 end)
